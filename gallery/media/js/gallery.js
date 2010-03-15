3b8f5881a9cbb7f06f65f15eb7151d2ccb9ae8b3
kat <kat@codebykat.com>
2010-03-15 17:19:47 -0400

    index loading images, style fixes

--- gallery/media/js/gallery.js
+++ gallery/media/js/gallery.js
@@ -1,4 +1,18 @@
 function setupGallery() {
+  $('div.thumbnail').each( function(i){
+    $(this).click(toggleImage);
+  });
 }
 
-addLoadEvent(setupGallery);
+function toggleImage() {
+  var id = this.id.substr(10);
+  var photo_id = "photo-" + id;
+  console.info(photo_id);
+  console.info($('div[id='+photo_id+']'));
+  //alert($('div[id=photo_id]'));
+  $('div.photo:visible').hide();
+  $('#'+photo_id).show();
+}
+
+jQuery(setupGallery());
+//$(document).ready(setupGallery());
