9fdfcea90e686dcf05d16e8114be3cbe7183607f
kat <kat@codebykat.com>
2010-03-17 13:04:39 -0400

    adjusting filmstrip to # of images

--- gallery/media/js/gallery.js
+++ gallery/media/js/gallery.js
@@ -6,16 +6,15 @@ function setupGallery() {
 
   // size filmstrip to number of photos
   var numPhotos = thumbs.size();
-  //var width = thumbs.get(0).width();
-  $('.thumbs').width(numPhotos*83);
-  //console.info(width);
+  var width = thumbs.outerWidth(true);
+  console.info(width);
+  $('.thumbs').width(numPhotos*width);
   //$('.thumbs').css('width',numPhotos*83+'px');
 }
 
 function toggleImage() {
   var id = this.id.substr(10);
   var photo_id = "photo-" + id;
-  console.info(photo_id);
   console.info($('div[id='+photo_id+']'));
   //alert($('div[id=photo_id]'));
   $('div.photo:visible').hide();
