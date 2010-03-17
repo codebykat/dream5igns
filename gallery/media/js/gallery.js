5d0f74c3ffe0a089bb3407b31cb803547de99f32
kat <kat@codebykat.com>
2010-03-17 13:50:58 -0400

    added scroll arrows to photostrip

--- gallery/media/js/gallery.js
+++ gallery/media/js/gallery.js
@@ -7,19 +7,31 @@ function setupGallery() {
   // size filmstrip to number of photos
   var numPhotos = thumbs.size();
   var width = thumbs.outerWidth(true);
-  console.info(width);
   $('.thumbs').width(numPhotos*width);
-  //$('.thumbs').css('width',numPhotos*83+'px');
 }
 
 function toggleImage() {
   var id = this.id.substr(10);
   var photo_id = "photo-" + id;
-  console.info($('div[id='+photo_id+']'));
-  //alert($('div[id=photo_id]'));
   $('div.photo:visible').hide();
   $('#'+photo_id).show();
 }
 
+function scrollStrip(multiplier) {
+  // multiplier -1 will scroll left, 1 will scroll right
+
+  var photoWidth = 83;
+  var scroll = photoWidth * multiplier;
+
+  var scrollGoal = $('#photostrip').scrollLeft() + scroll;
+  $('#photostrip').animate({scrollLeft: scrollGoal}, 200);
+}
+
+function setupScroll() {
+  $('#navleft').click(function() {scrollStrip(-1);});
+  $('#navright').click(function() {scrollStrip(1);});
+}
+
 jQuery(setupGallery());
+jQuery(setupScroll());
 //$(document).ready(setupGallery());
